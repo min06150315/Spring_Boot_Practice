91d8e1131b0f51a472b82e89c48d140487c482df
min06150315 <min06150315@gmail.com>
2024-11-10 05:19:33 +0900

    Spring Boot으로 만든 간단한 League of Legends CRUD Project

---
+++ spring-lol/src/main/java/com/leagueoflegends/legend/LegendApplication.java
@@ -0,0 +1,13 @@
+package com.leagueoflegends.legend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendApplication.class, args);
+	}
+
+}
---
+++ spring-lol/src/test/java/com/leagueoflegends/legend/LegendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leagueoflegends.legend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

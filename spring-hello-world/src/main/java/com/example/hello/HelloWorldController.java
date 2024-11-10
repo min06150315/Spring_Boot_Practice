265605d4c05ae6f2d9557851bbe8db761c5ce50e
min06150315 <min06150315@gmail.com>
2024-11-11 01:49:29 +0900

    스프링부트로 만든 간단한 Hello World 예시

--- spring-hello-world/src/main/java/com/example/hello/HelloWorldController.java
+++ spring-hello-world/src/main/java/com/example/hello/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World CRA!!";
+        return "Hello, World!";
     }
 }
--- spring-hello-world/src/main/java/com/example/hello/SpringHelloWorldApplication.java
+++ spring-hello-world/src/main/java/com/example/hello/SpringHelloWorldApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringHelloWorldApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringHelloWorldApplication.class, args);
 	}
